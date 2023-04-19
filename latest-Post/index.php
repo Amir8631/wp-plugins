655e7a302fc59b8452dad5188755f0113df6cb29
Amir8631 <110726949+Amir8631@users.noreply.github.com>
2023-04-19 17:42:38 +0330

    Add files via upload

---
+++ latest-Post/index.php
@@ -0,0 +1,78 @@
+<?php
+
+
+/*
+Plugin Name: Latest Posts
+Description: this is just little practice
+Version: 1.0
+Author: Amir
+*/
+
+
+function wpcat_postsbycategory() {
+
+
+$the_query = new WP_Query( array( 'category_name' => 'categoryslug', 'posts_per_page' => 10 ) ); 
+
+
+
+if ( $the_query->have_posts() ) {
+
+
+    $string .= '<ul class="postsbycategory widget_recent_entries">';
+
+
+    while ( $the_query->have_posts() ) {
+
+
+        $the_query->the_post();
+
+            if ( has_post_thumbnail() ) {
+            
+
+            $string .= '<li>';
+
+            $string .= '<a href="' . get_the_permalink() .'" rel="bookmark">' . get_the_post_thumbnail($post_id, array( 50, 50) ) . get_the_title() .'</a></li>';
+
+            } else { 
+
+
+            $string .= '<li><a href="' . get_the_permalink() .'" rel="bookmark">' . get_the_title() .'</a></li>';
+
+
+            }
+            }
+    } else {
+
+
+    // no posts found
+}
+
+
+$string .= '</ul>';
+
+
+ 
+
+
+return $string;
+
+
+wp_reset_postdata();
+
+
+}
+
+
+add_shortcode('categoryposts', 'wpcat_postsbycategory');
+
+add_filter('widget_text', 'do_shortcode');
+
+
+ 
+
+
+
+
+
+?>
